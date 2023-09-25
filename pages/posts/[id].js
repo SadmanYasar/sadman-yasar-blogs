cbec2781b15fc1711cc4df51a238e8b0a06d076b
Sadman Yasar Sayem <sadmanyasarsayem@gmail.com>
2023-09-25 10:44:08 +0800

    added custom scrollbar

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -3,6 +3,7 @@ import Date from '../../components/date'
 import Layout from '../../components/layout'
 import { getAllPostIds, getPostData } from '../../lib/posts'
 import Head from 'next/head'
+import { motion, useScroll, useSpring } from 'framer-motion'
 /* import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 import ReactMarkdown from 'react-markdown'
 
@@ -12,23 +13,45 @@ const CodeBlock = ({ language, value }) => {
 
 export default function Post({ postData }) {
     return (
-        <Layout>
-            <Head>
-                <title>{postData.title}</title>
-            </Head>
-            <article>
-                <h1 className={utilStyles.headingXl}>{postData.title}</h1>
-                <div className={utilStyles.lightText}>
-                    <Date dateString={postData.date} />
-                </div>
-                <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
-                {/* <ReactMarkdown
+        <>
+            <ScrollBar />
+            <Layout>
+                <Head>
+                    <title>{postData.title}</title>
+                </Head>
+                <article>
+                    <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+                    <div className={utilStyles.lightText}>
+                        <Date dateString={postData.date} />
+                    </div>
+                    <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+                    {/* <ReactMarkdown
                     escapeHtml={false}
                     children={postData.contentHtml}
                     renderers={{ code: CodeBlock }}
                 /> */}
-            </article>
-        </Layout>
+                </article>
+            </Layout>
+        </>
+    )
+}
+
+function ScrollBar() {
+    const { scrollYProgress } = useScroll()
+
+    const scaleX = useSpring(scrollYProgress, {
+        stiffness: 100,
+        damping: 30,
+        restDelta: 0.001
+    });
+
+    return (
+        <motion.div
+            className='fixed top-0 left-0 right-0 h-1 bg-purple-500 transform origin-left'
+            style={{
+                scaleX
+            }}
+        />
     )
 }
 
