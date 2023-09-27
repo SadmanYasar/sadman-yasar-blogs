899291f57621bc884f62221d7b122136a13423d3
Restyled.io <commits@restyled.io>
2023-09-27 06:02:44 +0000

    Restyled by prettier

--- pages/index.js
+++ pages/index.js
@@ -1,40 +1,54 @@
-import Date from '@/components/date'
-import Layout, {siteTitle} from '@/components/layout'
-import {getSortedPostsData} from '@/utils/mdxUtils'
-import {motion} from 'framer-motion'
-import Head from 'next/head'
-import Link from 'next/link'
+import Date from "@/components/date";
+import Layout, { siteTitle } from "@/components/layout";
+import { getSortedPostsData } from "@/utils/mdxUtils";
+import { motion } from "framer-motion";
+import Head from "next/head";
+import Link from "next/link";
 
 export async function getStaticProps() {
-  const allPostsData = getSortedPostsData()
+  const allPostsData = getSortedPostsData();
   return {
     props: {
       allPostsData,
     },
-  }
+  };
 }
 
-export default function Home({allPostsData}) {
-
+export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <Head>
         <title>{siteTitle}</title>
       </Head>
-      <section className='headingMd'>
-        <p>Hi! I am a Computer Science undergraduate student at UTM. Here, you will find my thoughts and anything that I find interesting to write on, mostly tech-related. 👨‍💻</p>
+      <section className="headingMd">
+        <p>
+          Hi! I am a Computer Science undergraduate student at UTM. Here, you
+          will find my thoughts and anything that I find interesting to write
+          on, mostly tech-related. 👨‍💻
+        </p>
       </section>
-      <section className='headingMd padding1px'>
-        <h2 className='headingLg'>Blog</h2>
-        <ul className='list'>
+      <section className="headingMd padding1px">
+        <h2 className="headingLg">Blog</h2>
+        <ul className="list">
           {allPostsData.map((post, index) => (
-            <motion.li className='listItem' key={post.filePath} initial={{ opacity: 0 }} animate={{ opacity: 1, transition: { delay: index * 0.5, duration: 1 } }}>
-              <Link legacyBehavior as={`/posts/${
-    post.filePath.replace(/\.mdx?$/, '')}`} href={`/posts/[slug]`}>
+            <motion.li
+              className="listItem"
+              key={post.filePath}
+              initial={{ opacity: 0 }}
+              animate={{
+                opacity: 1,
+                transition: { delay: index * 0.5, duration: 1 },
+              }}
+            >
+              <Link
+                legacyBehavior
+                as={`/posts/${post.filePath.replace(/\.mdx?$/, "")}`}
+                href={`/posts/[slug]`}
+              >
                 <a>{post.data.title}</a>
               </Link>
               <br />
-              <small className='lightText'>
+              <small className="lightText">
                 <Date dateString={post.data.date} />
               </small>
             </motion.li>
@@ -42,5 +56,5 @@ export default function Home({allPostsData}) {
         </ul>
       </section>
     </Layout>
-  )
+  );
 }
