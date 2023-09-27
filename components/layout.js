166aaa9cea9bb2c6632b1fda8168eb8bea96dfa3
Restyled.io <commits@restyled.io>
2023-09-27 06:15:14 +0000

    Restyled by clang-format

--- components/layout.js
+++ components/layout.js
@@ -1,37 +1,38 @@
 import Starfield from "@/components/starfield";
-import { motion } from "framer-motion";
+import {motion} from "framer-motion";
 import Head from "next/head";
 import Link from "next/link";
 
 export const siteTitle = "Sadman Yasar Sayem Blogs";
 const data = {
-  name: "Sadman Yasar Sayem",
-  profile: "https://avatars.githubusercontent.com/u/67522140?v=4",
-  urls: [
+  name : "Sadman Yasar Sayem",
+  profile : "https://avatars.githubusercontent.com/u/67522140?v=4",
+  urls : [
     {
-      iconPath: "/upwork.svg",
-      url: "https://www.upwork.com/freelancers/~01cfd344d945d1f282?viewMode=1",
+      iconPath : "/upwork.svg",
+      url : "https://www.upwork.com/freelancers/~01cfd344d945d1f282?viewMode=1",
     },
     {
-      iconPath: "/linkedin.svg",
-      url: "https://www.linkedin.com/in/Sadman-Yasar-Sayem/",
+      iconPath : "/linkedin.svg",
+      url : "https://www.linkedin.com/in/Sadman-Yasar-Sayem/",
     },
-    { iconPath: "/github.svg", url: "https://github.com/SadmanYasar" },
-    { iconPath: "/behance.svg", url: "https://www.behance.net/sadmanyasar" },
+    {iconPath : "/github.svg", url : "https://github.com/SadmanYasar"},
+    {iconPath : "/behance.svg", url : "https://www.behance.net/sadmanyasar"},
   ],
 };
 
-export default function Layout({ children, home }) {
+export default function Layout({children, home}) {
   return (
     <>
       <div className="max-w-2xl w-full min-h-screen mx-auto px-4">
         <Head>
           <link rel="icon" href="/favicon.ico" />
           <meta
-            name="description"
+  name = "description"
             content="Sadman Yasar Sayem personal website using Next.js"
           />
-          <meta property="og:image" content={data.profile} />
+          <meta property="og:image" content={
+    data.profile} />
           <meta name="og:title" content={siteTitle} />
           <meta name="twitter:card" content="summary_large_image" />
         </Head>
@@ -53,21 +54,20 @@ export default function Layout({ children, home }) {
                 {data.urls.map((url, index) => (
                   <a href={url.url} key={index} target="_blank">
                     <img
-                      src={url.iconPath}
-                      className="w-12 h-12 rounded-lg object-cover p-2 hover:shadow-lg hover:shadow-purple-500 transition duration-100"
-                    />
-                  </a>
+            src = {url.iconPath} className =
+                "w-12 h-12 rounded-lg object-cover p-2 hover:shadow-lg hover:shadow-purple-500 transition duration-100" /
+                > </a>
                 ))}
               </div>
-            </>
+                </>
           ) : (
             <>
               <Link legacyBehavior href="/">
-                <a>
-                  <img
-                    src={data.profile}
-                    className="rounded-full h-24 w-24 object-cover mt-8 transition hover:border-4 border-purple-500 border-opacity-20 ease-in-out duration-100"
-                    alt={data.name}
+                 < a>< img
+            src = {data.profile} className =
+                "rounded-full h-24 w-24 object-cover mt-8 transition hover:border-4 border-purple-500 border-opacity-20 ease-in-out duration-100"
+                    alt={
+    data.name}
                   />
                 </a>
               </Link>
